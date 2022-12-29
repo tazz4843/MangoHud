263c5df358ff088e6a2fa1abd59f5bade347a6ae
Kurt Kartaltepe <kkartaltepe@gmail.com>
2022-12-29 13:54:31 -0800

    Add perf counters based metrics for intel platforms
    Unlike AMD or Nvidia intel historically only exposes gpu telemetry with
    linux perf counters. As with all perf counters on modern linux these
    require root or CAP_PERFMON.
    
    This adds a small helper program that can be setuid/setcap'ed with the
    appropriate permissions to export the telemetry that mangohud is
    interested in. Most of the gpu metrics were informed by the existing
    intel_gpu_top tool we are using, however this also exports power metrics
    that the intel_gpu_top tool does not support for dgpu configurations and
    ensures that we select the appropriate card for all metrics in a
    multi-gpu system.
    
    Also this tool exports CPU power metrics from the perf counters. This
    allows power monitoring for Intel and zen+ AMD cpus. But this isnt
    hooked in this PR since it seems cpu and gpu metrics collection are
    separated in the rest of the mangohud.
    
    Mostly this is just a PoC to show what avoiding taking a third-party
    dependency on the rarely packaged intel_gpu_top might require. The meson
    setup in particular is just something thrown together that builds.

--- src/intel.cpp
+++ src/intel.cpp
@@ -7,64 +7,54 @@ using json = nlohmann::json;
 
 static bool init_intel = false;
 struct gpuInfo gpu_info_intel {};
+static char drm_dev[10];
 
 static void intelGpuThread(){
     init_intel = true;
-    static char stdout_buffer[1024];
-    FILE* intel_gpu_top = popen("intel_gpu_top -J -s 500", "r");
-    int num_line = 0;
-    std::string buf;
-    int num_iterations = 0;
-    while (fgets(stdout_buffer, sizeof(stdout_buffer), intel_gpu_top)) {
-        if (num_line > 0)
-            buf += stdout_buffer;
+    static char stdout_buffer[4096];
 
-        num_line++;
-        if (strlen(stdout_buffer) < 4 && !strchr(stdout_buffer, '{') && !strchr(stdout_buffer, ',')) {
-            if (buf[0] != '{')
-                buf = "{\n" + buf;
-
-            if (num_iterations > 0){
-                buf += "\n}";
-                json j = json::parse(buf);
-                if  (j.contains("engines"))
-                    if (j["engines"].contains("Render/3D/0"))
-                        if (j["engines"]["Render/3D/0"].contains("busy"))
-                            gpu_info_intel.load = j["engines"]["Render/3D/0"]["busy"].get<int>();
-
-                if (j.contains("frequency"))
-                    if (j["frequency"].contains("actual"))
-                        gpu_info_intel.CoreClock = j["frequency"]["actual"].get<int>();
-                if (j.contains("power")){
-                    if (j["power"].contains("GPU"))
-                        gpu_info_intel.powerUsage = j["power"]["GPU"].get<float>();
-                    if (j["power"].contains("Package"))
-                        gpu_info_intel.apu_cpu_power = j["power"]["Package"].get<float>();
-                }
-
-            }
-            buf = "";
-            num_line = 0;
+    std::string cmd("mango_intel_stats ");
+    cmd += drm_dev;
+    FILE* mango_intel_stats = popen(cmd.c_str() , "r");
+    while (fgets(stdout_buffer, sizeof(stdout_buffer), mango_intel_stats)) {
+        if ( stdout_buffer[0] != '{' || stdout_buffer[strlen(stdout_buffer)-1] != '\n') {
+            SPDLOG_ERROR("Overran 4k buffer for fgets output. Expect sadness:\n {}", stdout_buffer);
+            continue;
         }
-        num_iterations++;
+
+        json j = json::parse(stdout_buffer);
+        if  (j.contains("gpu_busy_pct"))
+            gpu_info_intel.load = j["gpu_busy_pct"].get<int>();
+        if  (j.contains("gpu_clock_mhz"))
+            gpu_info_intel.CoreClock = j["gpu_clock_mhz"].get<int>();
+        // Only one of igpu or dgpu should be present.
+        if  (j.contains("dgpu_power_w"))
+            gpu_info_intel.powerUsage = j["dgpu_power_w"].get<float>();
+        if  (j.contains("igpu_power_w"))
+            gpu_info_intel.powerUsage = j["igpu_power_w"].get<float>();
+        if  (j.contains("cpu_power_w"))
+            gpu_info_intel.apu_cpu_power = j["cpu_power_w"].get<float>();
     }
 
-    int exitcode = pclose(intel_gpu_top) / 256;
+    int exitcode = pclose(mango_intel_stats) / 256;
     if (exitcode > 0){
         if (exitcode == 127)
-        SPDLOG_INFO("Failed to open '{}'", "intel_gpu_top");
+            SPDLOG_INFO("Failed to open '{}'", "mango_intel_stats");
 
         if (exitcode == 1)
-        SPDLOG_INFO("Missing permissions for '{}'", "intel_gpu_top");
+            SPDLOG_INFO("Missing permissions for '{}'", "mango_intel_stats");
 
         SPDLOG_INFO("Disabling gpu_stats");
         _params->enabled[OVERLAY_PARAM_ENABLED_gpu_stats] = false;
     }
 }
 
-void getIntelGpuInfo(){
-    if (!init_intel)
+void getIntelGpuInfo(const char *_drm_dev){
+    if (!init_intel) {
+        assert(strlen(_drm_dev) < sizeof(drm_dev)-1);
+        strcpy(drm_dev,  _drm_dev);
         std::thread(intelGpuThread).detach();
+    }
 
     gpu_info = gpu_info_intel;
 }
